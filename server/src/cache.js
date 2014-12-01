516090717f3d01cb317165034cb3b40d290489f9
Frederick Gnodtke <frederick@gnodtke.net>
2014-12-01 17:31:07 +0100

    Added License and license headers

--- server/src/cache.js
+++ server/src/cache.js
@@ -1,3 +1,20 @@
+/*
+ *  This file is part of CORPSES, a webinterface for 7 Days to Die.
+ *
+ *  CORPSES is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CORPSES is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CORPSES. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function Cache(server) {
 	this.time = undefined;
 	this.knownPlayers = undefined;
