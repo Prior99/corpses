9c8014b30f7bc7dc5143672eea91c6071c17d5e1
Frederick Gnodtke <frederick@gnodtke.net>
2014-12-03 17:21:42 +0100

    Added testcases for register

--- server/tests/test_client.js
+++ server/tests/test_client.js
@@ -45,9 +45,13 @@ describe("The interface to the client", function(done) {
 
 		}
 	};
+
 	var database = new Database(dbConfig);
 	var mockServer = {
-
+		newUser : false,
+		notifyNewUser : function() {
+			mockServer.newUser = true;
+		}
 	};
 	var client = new Client(mockWebsocket, database, mockServer);
 	describe("The general system", function() {
@@ -88,8 +92,27 @@ describe("The interface to the client", function(done) {
 		it("can register a non-existing account", function(done) {
 			callMockedListener("register", {name : "Test5", steamid : "56789", password : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"}, function(answer) {
 				answer.okay.should.be.true;
+				mockServer.newUser.should.be.true;
+				database.getUserByName("Test5", function(err, user) {
+					user.name.should.equal("Test5");
+					user.steamid.should.equal("56789");
+					user.password.should.equal("a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3");
+					user.enabled.should.eql(0);
+					done();
+				});
+			});
+		});
+		it("can't register an existing account", function(done) {
+			callMockedListener("register", {name : "Test5", steamid : "56789", password : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"}, function(answer) {
+				answer.okay.should.be.false;
 				done();
 			});
-		})
+		});
+		it("can't register an account with a name shorter than 3 characters", function(done) {
+			callMockedListener("register", {name : "Ah", steamid : "56789", password : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"}, function(answer) {
+				answer.okay.should.be.false;
+				done();
+			});
+		});
 	});
 });
