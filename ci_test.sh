4ffe5a3421464a0493555cf15af39644e8c3161b
Prior <fgnodtke@cronosx.de>
2014-12-04 10:15:38 +0100

    I hope that the ci_test.sh will now fail the build if an error was detected. Also reduced verbosity.

--- ci_test.sh
+++ ci_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-npm install | iconv -tascii -c
-echo "n" | bower install | iconv -tascii -c
+echo -n "Installing npm components ... "
+npm install | iconv -tascii -c > npm.log
+echo "Done."
+echo -n "Installing bower components ... "
+echo "n" | bower install | iconv -tascii -c > bower.log
+echo "Done."
 cp server/config.json.example server/config.json
-grunt | iconv -tascii -c
+echo "Now building/testing ... "
+grunt | iconv -tascii -c | tee grunt.log
+grep --quiet "failing" grunt.log
+exit $?
