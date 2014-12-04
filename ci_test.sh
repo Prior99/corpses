4c8d6a073db87e65dedfeee2cdd8fcdaf0d1b825
Prior <fgnodtke@cronosx.de>
2014-12-04 10:34:16 +0100

    Another attempt to fail builds correctly. Another attempt to geet npm to shut the hell up.

--- ci_test.sh
+++ ci_test.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 echo -n "Installing npm components ... "
-npm install | iconv -tascii -c &> npm.log
+npm install --silent | iconv -tascii -c &> npm.log
 echo "Done."
 echo -n "Installing bower components ... "
-echo "n" | bower install | iconv -tascii -c &> bower.log
+echo "n" | bower install --silent | iconv -tascii -c &> bower.log
 echo "Done."
 cp server/config.json.example server/config.json
 echo "Now building/testing ... "
-grunt | iconv -tascii -c | tee grunt.log
+grunt | tee grunt.log
 grep --quiet "failing" grunt.log
-exit $?
+RESULT=$?
+if [ "$RESULT" = "1" ]; then
+	echo "The build was successfull."
+	exit 0
+else
+	echo "Some tests were detected to have failed."
+	exit 1
+fi
---
+++ server/tests/test.fail.js
@@ -0,0 +1,5 @@
+describe("A test that should go wrong", function() {
+	it("should go wrong", function() {
+		true.should.be.false;
+	});
+});
