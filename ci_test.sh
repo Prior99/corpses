7883e516e4eed2472c67cb3f8e10e34e03ff6e82
Prior <fgnodtke@cronosx.de>
2014-12-04 10:24:03 +0100

    Removed failing test as it worked. Another attempt to reduce npm's verbosity in ci_test.sh

--- ci_test.sh
+++ ci_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo -n "Installing npm components ... "
-npm install | iconv -tascii -c > npm.log
+npm install | iconv -tascii -c &> npm.log
 echo "Done."
 echo -n "Installing bower components ... "
-echo "n" | bower install | iconv -tascii -c > bower.log
+echo "n" | bower install | iconv -tascii -c &> bower.log
 echo "Done."
 cp server/config.json.example server/config.json
 echo "Now building/testing ... "
