bc20b4db80d5397f9b91df687bab6d01b35e1bda
Frederick Gnodtke <frederick@gnodtke.net>
2014-12-05 23:00:27 +0100

    Refactored code to remove useless testcases and merge them repsectivley with other ones

--- server/tests/client/gamequeries.js
+++ server/tests/client/gamequeries.js
@@ -20,32 +20,39 @@ module.exports = function(client1, client2, database, server, mockSock1, mockSoc
 				done();
 			});
 		});
-		it("can login a second user at the same time", function(done) {
+		it("can fetch the extended players and sees his friends", function(done) {
 			mockSock2.callMockedListener("login", {name : "Test3", password : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"},
 				function(answer) {
 					assert(answer.okay);
-					done();
+					mockSock1.callMockedListener("addFriend", client2.user.steamid, function() {
+						mockSock2.callMockedListener("addFriend", client1.user.steamid, function() {
+							database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
+								assert(okay && !err);
+								mockSock1.callMockedListener("getPlayers", null, function(answer) {
+									assert(answer.okay);
+									assert(answer.players.length == 2);
+									done();
+								});
+							});
+						});
+					});
 				}
 			);
 		});
-		it("can add a friend", function(done) {
-			mockSock1.callMockedListener("addFriend", client2.user.steamid, function() {
-				mockSock2.callMockedListener("addFriend", client1.user.steamid, function() {
-					done();
+		it("is not seen by non-friends", function(done) {
+			mockSock1.callMockedListener("removeFriend", client2.user.steamid, function() {
+				database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
+					assert(!okay && !err);
+					mockSock1.callMockedListener("getPlayers", null, function(answer) {
+						assert(answer.okay);
+						assert(answer.players.length == 1);
+						mockSock2.callMockedListener("getPlayers", null, function(answer) {
+							assert(answer.okay);
+							assert(answer.players.length == 1);
+							done();
+						});
+					});
 				});
-			});
-		});
-		it("are friends afterwards", function(done) {
-			database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
-				assert(okay && !err);
-				done();
-			});
-		});
-		it("can fetch the extended players and sees his friends", function(done) {
-			mockSock1.callMockedListener("getPlayers", null, function(answer) {
-				assert(answer.okay);
-				assert(answer.players.length == 2);
-				done();
 			});
 		});
 	});
--- server/tests/client/markers.js
+++ server/tests/client/markers.js
@@ -2,27 +2,24 @@ var assert = require("assert");
 
 module.exports = function(client1, client2, database, server, mockSock1, mockSock2) {
 	describe("Adding Markers", function() {
-		it("can login a second user at the same time", function(done) {
+		it("can add a public marker", function(done) {
 			mockSock2.callMockedListener("login", {name : "Test3", password : "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"},
 				function(answer) {
 					assert(answer.okay);
-					done();
-				}
-			);
-		});
-		it("can add a public marker", function(done) {
-			mockSock1.callMockedListener("addMarker", {
-				name : "Testmarker 1",
-				lat : 14.3,
-				lng : -3.4,
-				description : "This is a testmarker",
-				icon : "fa-trash",
-				visibility : 'public'}, function(answer) {
-					assert(answer.okay);
-					var msg = mockSock2.lastMsg;
-					assert(msg !== undefined);
-					assert(msg.key === "marker");
-					done();
+					mockSock1.callMockedListener("addMarker", {
+						name : "Testmarker 1",
+						lat : 14.3,
+						lng : -3.4,
+						description : "This is a testmarker",
+						icon : "fa-trash",
+						visibility : 'public'}, function(answer) {
+							assert(answer.okay);
+							var msg = mockSock2.lastMsg;
+							assert(msg !== undefined);
+							assert(msg.key === "marker");
+							done();
+						}
+					);
 				}
 			);
 		});
@@ -41,65 +38,53 @@ module.exports = function(client1, client2, database, server, mockSock1, mockSoc
 				}
 			);
 		});
-		it("can remove a friend", function(done) {
+		it("can add a friends-only marker and not-friends can not see it", function(done) {
 			mockSock1.callMockedListener("removeFriend", client2.user.steamid, function() {
 				mockSock2.callMockedListener("removeFriend", client1.user.steamid, function() {
-					done();
+					database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
+						assert(!okay && !err);
+						delete mockSock2.lastMsg;
+						mockSock1.callMockedListener("addMarker", {
+							name : "Testmarker 3",
+							lat : 10.3,
+							lng : -90.4,
+							description : "This is a testmarker",
+							icon : "fa-trash",
+							visibility : 'friends'}, function(answer) {
+								assert(answer.okay);
+								var msg = mockSock2.lastMsg;
+								assert(msg === undefined);
+								done();
+							}
+						);
+					});
 				});
-			});
-		});
-		it("are not friends afterwards", function(done) {
-			database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
-				assert(!okay && !err);
-				done();
-			});
-		});
-		it("can add a friends-only marker and not-friends can not see it", function(done) {
-			delete mockSock2.lastMsg;
-			mockSock1.callMockedListener("addMarker", {
-				name : "Testmarker 3",
-				lat : 10.3,
-				lng : -90.4,
-				description : "This is a testmarker",
-				icon : "fa-trash",
-				visibility : 'friends'}, function(answer) {
-					assert(answer.okay);
-					var msg = mockSock2.lastMsg;
-					assert(msg === undefined);
-					done();
-				}
-			);
-		});
-		it("can add a friend", function(done) {
-			mockSock1.callMockedListener("addFriend", client2.user.steamid, function() {
-				mockSock2.callMockedListener("addFriend", client1.user.steamid, function() {
-					done();
-				});
-			});
-		});
-		it("are friends afterwards", function(done) {
-			database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
-				assert(okay && !err);
-				done();
 			});
 		});
 		it("can add a friends-only marker and friends can see it", function(done) {
-			mockSock2.lastMsg = undefined;
-			var marker = {
-				name : "Testmarker 4",
-				lat : 11.3,
-				lng : -9.4,
-				description : "This is a testmarker",
-				icon : "fa-trash",
-				visibility : 'friends'
-			};
-			mockSock1.callMockedListener("addMarker", marker, function(answer) {
-					assert(answer.okay);
-					var msg = mockSock2.lastMsg;
-					assert(msg !== undefined);
-					done();
-				}
-			);
+			mockSock1.callMockedListener("addFriend", client2.user.steamid, function() {
+				mockSock2.callMockedListener("addFriend", client1.user.steamid, function() {
+					database.areFriends(client1.user.id, client2.user.id, function(err, okay) {
+						assert(okay && !err);
+						mockSock2.lastMsg = undefined;
+						var marker = {
+							name : "Testmarker 4",
+							lat : 11.3,
+							lng : -9.4,
+							description : "This is a testmarker",
+							icon : "fa-trash",
+							visibility : 'friends'
+						};
+						mockSock1.callMockedListener("addMarker", marker, function(answer) {
+								assert(answer.okay);
+								var msg = mockSock2.lastMsg;
+								assert(msg !== undefined);
+								done();
+							}
+						);
+					});
+				});
+			});
 		});
 		var markerID;
 		it("can fetch markers and see it's own markers", function(done) {
