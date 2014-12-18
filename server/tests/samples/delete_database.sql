6666b6c230e589238c7801ca9f23823797dda561
Frederick Gnodtke <frederick@gnodtke.net>
2014-12-18 16:27:03 +0100

    ingore foreign key constraints on drop

--- server/tests/samples/delete_database.sql
+++ server/tests/samples/delete_database.sql
@@ -1,5 +1,7 @@
+SET foreign_key_checks = 0;
 DROP TABLE IF EXISTS `Admins`;
 DROP TABLE IF EXISTS `Friends`;
 DROP TABLE IF EXISTS `MarkerIgnore`;
 DROP TABLE IF EXISTS `Markers`;
 DROP TABLE IF EXISTS `Users`;
+SET foreign_key_checks = 1;
