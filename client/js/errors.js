1f9439bbef5e240de3319a7fdb4940e88b7033c3
Andra <andra@ruebsteck.de>
2015-02-07 19:51:23 +0100

    Refined error messages

--- client/js/errors.js
+++ client/js/errors.js
@@ -18,7 +18,7 @@
 Errors = {
 	"internal_error" : {
 		name : "Internal server error",
-		description : "An internal server error occured. Please contact the system administrator about this incident. And provide him steps about how to reproduce this problem. To continue, try reloading the page, refreshing your login and deleting all cookies and browsercache."
+		description : "An internal server error occured. Please contact the system administrator about this incident and provide him with steps about how to reproduce this problem. To continue, try reloading the page, deleting all cookies and the browser cache and logging off and on again."
 	},
 	"no_login" : {
 		name : "Not logged in",
@@ -26,19 +26,19 @@ Errors = {
 	},
 	"user_unknown" : {
 		name : "User unknown",
-		description : "The user you are specifying does not exist or is at least not known by the server."
+		description : "The user you are specifying does not exist or is at least not known to the server."
 	},
 	"invalid_arguments" : {
 		name : "Invalid input",
-		description : "The input you supplied was invalid. Please check that all neccessary fields are filled and the provided data is correct."
+		description : "The input you supplied was invalid. Please check that all necessary fields are filled and the provided data is correct."
 	},
 	"username_taken_or_internal_error" : {
 		name : "Username taken",
 		description : "The username you chose was already taken. Please choose another username."
 	},
 	"username_or_password_wrong" : {
-		name : "Username and password don't match or account disabled",
-		description : "The username and password you provided did not match any user in the database. Please check, that both are correct. <i>Please note, that your account needs to be activated after it was registered or you were deactivated!</i>"
+		name : "Username and password don't match or account is disabled",
+		description : "The username and password you provided did not match any user in the database. Please check that both are correct. <i>Please note, that your account needs to be activated after it was registered or you were deactivated!</i>"
 	},
 	"no_admin" : {
 		name : "Not an admin",
@@ -46,7 +46,7 @@ Errors = {
 	},
 	"unknown_marker" : {
 		name : "Unknown marker",
-		description : "You supplied a marker that is not exitent."
+		description : "You supplied a marker that does not exist."
 	}
 };
 
